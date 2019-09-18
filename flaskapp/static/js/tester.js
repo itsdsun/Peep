b967d5024fc5ed35f63869f3ee1507e448da5a04
Laura Sun <sun.laura@outlook.com>
2019-09-17 23:39:45 -0700

    updated gif

--- flaskapp/static/js/tester.js
+++ flaskapp/static/js/tester.js
@@ -19,6 +19,18 @@ d3.json(url).then(function(response) {
 
       document.getElementById("data").innerHTML = temp;
     }
+});
 
+$("#myInput").keyup(function () {
+    var value = this.value.toLowerCase().trim();
 
+    $("table tr").each(function (index) {
+        if (!index) return;
+        $(this).find("td").each(function () {
+            var id = $(this).text().toLowerCase().trim();
+            var not_found = (id.indexOf(value) == -1);
+            $(this).closest('tr').toggle(!not_found);
+            return not_found;
+        });
+    });
 });
